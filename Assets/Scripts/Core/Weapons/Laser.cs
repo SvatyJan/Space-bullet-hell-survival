42dc6b1c7151a728448c3535c35123be1101c3b6
SvatyJan <honzikraj@gmail.com>
2025-03-23 20:42:11 +0100

    Laser weapon
    Laser weapon

---
+++ Assets/Scripts/Core/Weapons/Laser.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour, IWeapon
+{
+    [SerializeField] private float defaultRayDistance = 10f;
+    [SerializeField] private Transform laserFirePoint;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private LayerMask hitLayers;
+    [SerializeField] private List<string> damageableTags;
+    [SerializeField] private float damagePerSecond = 5f;
+
+    public SpaceEntity owner;
+
+    private void Awake()
+    {
+        if (lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        lineRenderer.startWidth = 0.2f;
+        lineRenderer.endWidth = 0.5f;
+        lineRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        owner = GetComponentInParent<SpaceEntity>();
+        if (owner == null)
+        {
+            Debug.LogError("Laser: Nelze najít vlastníka zbranì! Zbraò nebude fungovat.");
+            return;
+        }
+
+        if (laserFirePoint == null)
+        {
+            laserFirePoint = transform;
+        }
+    }
+
+    public void Fire()
+    {
+        Vector2 startPosition = laserFirePoint.position;
+        Vector2 endPosition = startPosition + (Vector2)laserFirePoint.up * defaultRayDistance;
+
+        RaycastHit2D hit = Physics2D.Raycast(startPosition, laserFirePoint.up, defaultRayDistance, hitLayers);
+
+        // Pokud je zásah a cíl je poškozovatelný
+        if (hit.collider != null && damageableTags.Contains(hit.collider.tag))
+        {
+            endPosition = hit.point;
+
+            // Aktivujeme laser pouze pokud nìco trefíme
+            if (!lineRenderer.enabled)
+                lineRenderer.enabled = true;
+
+            DrawLaserRay(startPosition, endPosition);
+
+            SpaceEntity target = hit.collider.GetComponent<SpaceEntity>();
+            if (target != null)
+            {
+                target.TakeDamage((damagePerSecond + owner.GetComponent<ShipStats>().BaseDamage) * Time.deltaTime);
+
+                // Pokud nepøítel je znièen, deaktivujeme laser
+                if (target.GetComponent<ShipStats>().CurrentHealth <= 0)
+                {
+                    lineRenderer.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            // Pokud nic netrefíme, laser se skryje
+            if (lineRenderer.enabled)
+                lineRenderer.enabled = false;
+        }
+    }
+
+    private void DrawLaserRay(Vector2 startPosition, Vector2 endPosition)
+    {
+        lineRenderer.SetPosition(0, startPosition);
+        lineRenderer.SetPosition(1, endPosition);
+    }
+
+    public void Upgrade() { throw new System.NotImplementedException(); }
+    public void Evolve() { throw new System.NotImplementedException(); }
+}
